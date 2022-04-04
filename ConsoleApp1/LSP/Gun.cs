b315e605852da646603297a393d644591c3a814f
mawangqing <1426314808@qq.com>
2022-04-04 22:49:47 +0800

    Lsp

---
+++ ConsoleApp1/LSP/Gun.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1.LSP
+{
+    /// <summary>
+    /// 枪支
+    /// </summary>
+    internal abstract class Gun
+    {
+        /// <summary>
+        /// 射击
+        /// </summary>
+        public abstract void Shoot();
+        /// <summary>
+        /// 装弹
+        /// </summary>
+        public abstract void Loading();
+        /// <summary>
+        /// 射程
+        /// </summary>
+        public abstract int Range { get; }
+    }
+    /// <summary>
+    /// 手枪
+    /// </summary>
+    internal class Pistol : Gun
+    {
+        public override void Loading()
+        {
+            Console.WriteLine("手枪装弹");
+        }
+
+        public override void Shoot()
+        {
+            Console.WriteLine("手枪啪啪啪啪啪");
+        }
+        /// <summary>
+        /// 射程
+        /// </summary> 
+
+        public override int Range => 50;
+    }
+    /// <summary>
+    /// 步枪
+    /// </summary>
+    internal class Rifle : Gun
+    {
+        public override void Loading()
+        {
+            Console.WriteLine("步枪装弹");
+        }
+
+        public override void Shoot()
+        {
+            Console.WriteLine("步枪突突突突突");
+        }
+        /// <summary>
+        /// 射程
+        /// </summary> 
+
+        public override int Range =>500;
+    }
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleApp1.LSP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var soldier = new Soldier();
+                soldier.SetGun(new Pistol());
+                soldier.KillEnemy();
+
+                Console.ReadKey();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
